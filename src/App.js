f171bb61ac08ff5fc9e99d0ba3e56a2ef1274313
Rayan <abdelmadjidzenine@gamil.com>
2022-02-09 13:06:03 +0100

    added Tool component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import Toolbar from './components/Toolbar/Toolbar';
+
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Tools from './components/Tools/Tools';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Toolbar/>
+      <Routes>
+        <Route path="/outils/*"  element={<Tools/>}/>
+      </Routes>
+    </BrowserRouter>
+    
   );
 }
 
